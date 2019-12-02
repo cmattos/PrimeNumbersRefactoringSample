ef8a98829c2054fd9ffef971b010745e3ec8a358
carlos.mattos <csms@gft.com>
2019-12-02 00:09:07 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrimeNumbersRefactoringSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = GeneratePrimes.GeneratePrimeNumbers(50);
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine(n[i].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
